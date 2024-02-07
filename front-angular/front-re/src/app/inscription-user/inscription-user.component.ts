2cdb69f253fdddc44fd782a3af07bd311e39bebf
Amine FODIL-CHERIF <afodil@ipanipan.com>
2024-02-07 15:59:28 +0100

    formulaire d'inscription + auto increment terminé

--- front-angular/front-re/src/app/inscription-user/inscription-user.component.ts
+++ front-angular/front-re/src/app/inscription-user/inscription-user.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http'; // Importez HttpHeaders depuis @angular/common/http
+import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
 import { Citizens } from './model/citizen.model';
 import { FormsModule } from '@angular/forms';
 
@@ -19,14 +19,12 @@ export class InscriptionUserComponent {
   onSubmit(): void {
     console.log(this.citizen); 
 
-    // Créez un objet HttpHeaders et ajoutez l'en-tête "Access-Control-Allow-Origin"
     const httpOptions = {
       headers: new HttpHeaders({
         'Access-Control-Allow-Origin': 'http://localhost:8080'
       })
     };
 
-    // Utilisez cet objet HttpHeaders dans la requête HTTP
     this.http.post<Citizens>('http://localhost:8080/citizen/createCitizen', this.citizen, httpOptions)
       .subscribe({
         next: (response) => console.log('Utilisateur créé avec succès !', response),
