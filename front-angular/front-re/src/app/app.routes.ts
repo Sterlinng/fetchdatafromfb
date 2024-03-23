b109719f7873bfb60f890dfaa96b06076afbe394
Emmanuel GARNIER BOIDUN <manu.boidun@gmail.com>
2024-03-23 12:01:11 +0100

    inscription-corp

--- front-angular/front-re/src/app/app.routes.ts
+++ front-angular/front-re/src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { InscriptionUserComponent } from './inscription-user/inscription-user.component';
+import { InscriptionCorpComponent } from './inscription-corp/inscription-corp.component';
 import { ConnexionUserComponent } from './connexion-user/connexion-user.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+    { path: 'inscription-corp', component: InscriptionCorpComponent },
     { path: 'inscription-user', component: InscriptionUserComponent },
     { path: 'connexion-user', component: ConnexionUserComponent },
-    { path: 'home', component: HomeComponent }
+    { path: 'home', component: HomeComponent },
 ];
+
+
